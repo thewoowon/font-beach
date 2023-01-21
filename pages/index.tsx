70757616d428b6d47deffde84235a4b0f09f6397
thewoowon <thewoowon@naver.com>
2023-01-21 16:07:34 +0900

    chore: Add Prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,10 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Layout from '@/components/Layout'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <Layout title="FontBeach - Home" description="Welcome to the FontBeach!">
-      <div></div>
+      <div>우리가</div>
     </Layout>
   )
 }
