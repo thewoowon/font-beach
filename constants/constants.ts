96fc0117129f7238ce5ca1b11bee7a8306eb154d
thewoowon <thewoowon@naver.com>
2023-02-14 13:04:36 +0900

    feat(constants): Add categories

--- constants/constants.ts
+++ constants/constants.ts
@@ -22,3 +22,42 @@ export const getOrderBy = (orderBy?: string) => {
       : { orderBy: { like: 'desc' } }
     : undefined
 }
+
+export const Categories: CategoryType[] = [
+  {
+    id: '1',
+    name: '최신',
+    slug: 'font',
+    description: '최신 폰트',
+    image: '/images/categories/font.png',
+    createdAt: '2021-01-01',
+    updatedAt: '2021-01-01',
+  },
+  {
+    id: '2',
+    name: '브랜드',
+    slug: 'theme',
+    description: '브랜드별 인기 폰트',
+    image: '/images/categories/theme.png',
+    createdAt: '2021-01-01',
+    updatedAt: '2021-01-01',
+  },
+  {
+    id: '3',
+    name: '트렌드',
+    slug: 'font',
+    description: '요즘 트렌드를 반영한 폰트',
+    image: '/images/categories/font.png',
+    createdAt: '2021-01-01',
+    updatedAt: '2021-01-01',
+  },
+  {
+    id: '4',
+    name: '연령순',
+    slug: 'theme',
+    description: '연령별 인기 폰트',
+    image: '/images/categories/theme.png',
+    createdAt: '2021-01-01',
+    updatedAt: '2021-01-01',
+  },
+]
